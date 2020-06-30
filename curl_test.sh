c298a434436a78647d8b3945d0794006170fb71f
kramkris <35402639+kramkris@users.noreply.github.com>
2020-06-30 09:39:28 +0530

    Update curl_test.sh

--- curl_test.sh
+++ curl_test.sh
@@ -1,3 +1,3 @@
 for i in {1..35}; do
-   kubectl exec --namespace=kube-public curl -- sh -c 'test=`wget -qO- -T 2  http://curlpagehere 2>&1` && echo "$test OK" || echo "Failed"';
+   kubectl exec --namespace=typenshere curl -- sh -c 'test=`wget -qO- -T 2  http://curlpagehere 2>&1` && echo "$test OK" || echo "Failed"';
    echo ""
